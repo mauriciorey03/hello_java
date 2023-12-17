4a6a597d603a4c76dbbbcbd74dcca9df48e0ced0
mauriciorey03 <edgarmeza909@gmail.com>
2023-12-17 12:47:36 -0500

    Update

---
+++ src/z_1apuntes.java
@@ -0,0 +1,9 @@
+public class z_1apuntes { // El nombre debe coincidir con el archivo
+    public static void main(String[] args) {
+        System.out.println("Hello World!"); 
+    }
+}
+
+// println(); envia el msg del archivo
+// Las llaves {} marcan el principio y el final de un bloque de código.
+//System es una clase Java incorporada que contiene miembros útiles, como out, que es la abreviatura de "salida". El método println(), abreviatura de "imprimir línea", se utiliza para imprimir un valor en la pantalla (o un archivo).
---
+++ src/z_2apuntes.java
@@ -0,0 +1,14 @@
+public class z_2apuntes {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo!");
+        System.out.print("Estoy aprendiendo Java.");
+        System.out.println("Es increible!");
+    }
+}
+
+// La diferencia entre print() y println() es que se hace salto de línea
+/*
+Este es un comentario
+de multiples líneas
+no afecta su funcionamiento
+ */
