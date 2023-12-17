366956d1bd06df4142cd883571b6a79390a77cec
mauriciorey03 <edgarmeza909@gmail.com>
2023-12-17 13:43:45 -0500

    Update

--- src/a_helloworld.java
+++ src/a_helloworld.java
@@ -1,6 +1,6 @@
 public class a_helloworld{
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    System.out.println("Hello my World!");
   }
 }
 
--- src/b_Saludo.java
+++ src/b_Saludo.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class b_Saludo {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         Scanner Leer = new Scanner (System.in);
         System.out.println("Ingrese su nombre: ");
         String nombre = Leer.next();
