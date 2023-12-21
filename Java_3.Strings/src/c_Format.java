980741637f92571890737857bea0fda73ea0a8eb
mauriciorey03 <edgarmeza909@gmail.com>
2023-12-21 00:35:49 -0500

    Estudio y organización temática

---
+++ Java_3.Strings/src/c_Format.java
@@ -0,0 +1,12 @@
+public class c_Format {
+    public static void main(String[] args) {
+			String mensaje = "";
+
+			mensaje= String.format("Bienvenido a %s","JAVA"); //NECESITA DE DOS ARG 
+
+			//%s, es reemplazado por Java
+
+        System.out.println(mensaje);
+
+    }
+}
