18a21901ca3d0cf6174bbf28fd5972a5e8849852
angie ardila <katali1296@gmail.com>
2024-01-03 20:09:27 -0500

    Clase enero

--- Java_4.Quizz/src/a_quizz1.java
+++ Java_4.Quizz/src/a_quizz1.java
@@ -1,19 +1,48 @@
+// import java.util.Scanner;
+
+// public class a_quizz1 {
+//     public static void main(String[] args) throws Exception {
+//         Scanner input = new Scanner(System.in);
+
+//         System.out.println("Ingrese la cantidad de días: ");
+//         int dia = input.nextInt();
+//         input.close();
+
+//         double alza =3.9;
+//         for (int i = 0; i < dia; i++) {
+//             System.out.print("Día "+(i+1)+": ");
+//             double valor = input.nextDouble();            
+//         }
+//         System.out.println("La mayor alza fue "+alza);
+
+//     } 
+// }
+
 import java.util.Scanner;
 
 public class a_quizz1 {
     public static void main(String[] args) throws Exception {
-        Scanner input = new Scanner(System.in);
+        Scanner leer = new Scanner(System.in);
 
-        System.out.println("Ingrese la cantidad de días: ");
-        int dia = input.nextInt();
-        input.close();
+        System.out.println("Ingrese el número de días: ");
+        int numDia = leer.nextInt();
+        double precioDolarHoy = 0;
+        double precioDolarAyer = 0;
+        double mayorAlza = 0;
 
-        double alza =3.9;
-        for (int i = 0; i < dia; i++) {
-            System.out.print("Día "+(i+1)+": ");
-            double valor = input.nextDouble();            
+
+        for (int i = 1; i <= numDia; i++) {
+            System.out.print("Día "+(i)+", ingrese precio del dolar: ");
+            precioDolarHoy = leer.nextDouble();
+            mayorAlza = precioDolarHoy;
+            double diferencia = Math.abs(precioDolarAyer - precioDolarHoy);
+            if (mayorAlza<diferencia){
+                mayorAlza = diferencia;
+            }
+            precioDolarAyer = precioDolarHoy;
         }
-        System.out.println("La mayor alza fue "+alza);
-
+        System.out.println("La mayor alza fue "+mayorAlza);
+        
+        leer.close();
     } 
 }
---
+++ Java_5.Exercises/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        System.out.println("Hello, World!");
+        
+        Scanner leer = new Scanner(System.in);
+
+    }
+}
